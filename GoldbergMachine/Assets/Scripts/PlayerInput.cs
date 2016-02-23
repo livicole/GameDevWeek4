5952cc7285fd2c92a97c0765d56099236e4d8ab0
Olivia Mignone <om651@nyu.edu>
2016-02-23 07:50:07 -0500

    Final build

--- GoldbergMachine/Assets/Scripts/PlayerInput.cs
+++ GoldbergMachine/Assets/Scripts/PlayerInput.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class PlayerInput : MonoBehaviour {
 
 	public GameObject door;
+	public GameObject cameraDoor;
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.Space)) {
 			Destroy (door);
+			Destroy (cameraDoor);
 		}
 	
 	}
